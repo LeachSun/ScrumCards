d0063ad6d61b09f8cbd8e587440592272b39da7e
sunlinqing <linqing_sun@lovelawu.com>
2018-06-11 17:57:51 +0800

    optimize master、member page

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -86,33 +86,36 @@ function CardWebsocket(url, openCallback, msgCallback, closeCallback) {
     }
 }
 
-/*
-function initWebsocket(url, revCallback) {
-    var websocket = new WebSocket(url);
 
-    websocket.onopen = function() {
-        console.log('connect');
-    };
-
-    websocket.onmessage = function(e) {
-        var msgJson = e.data;
-        console.log('rev: ' + msgJson);
-        if (revCallback != undefined) {
-            revCallback(JSON.parse(msgJson));
-        }
-
-    };
-
-    websocket.onclose = function() {
-        console.log('close reconnect');
-
-    };
-
-    return websocket;
+function hasClass(elements, cName) {
+    return !!elements.className.match(new RegExp("(\\s|^)" + cName + "(\\s|$)"));
 }
 
-function sendmsg(websocket, msgObj) {
-    var json = JSON.stringify(msgObj);
-    console.log('send: ' + json);
-    websocket.onsend(json);
-}*/
+function addClass(elements, cName) {
+    elements.className += " " + cName;
+}
+
+function removeClass(elements, cName) {
+    elements.className = elements.className.replace(new RegExp("(\\s|^)" + cName + "(\\s|$)"), " ");
+}
+
+function setCookie(c_name,value,expiredays) {
+    var exdate=new Date()
+    exdate.setDate(exdate.getDate()+expiredays)
+    document.cookie=c_name+ "=" +escape(value)+
+        ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+}
+function getCookie(c_name) {
+    if (document.cookie.length>0)
+    {
+        c_start=document.cookie.indexOf(c_name + "=")
+        if (c_start!=-1)
+        {
+            c_start=c_start + c_name.length+1
+            c_end=document.cookie.indexOf(";",c_start)
+            if (c_end==-1) c_end=document.cookie.length
+            return unescape(document.cookie.substring(c_start,c_end))
+        }
+    }
+    return ""
+}
